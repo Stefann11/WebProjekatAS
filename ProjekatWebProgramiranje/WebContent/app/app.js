1be3382b979c6d01ea9c9d28d100d213aa3cd507
Stefann11 <santrac.stefan@gmail.com>
2020-08-14 18:53:21 +0200

    Dodavanje vue.js i pokusaj povezivanja sa registration.html

--- ProjekatWebProgramiranje/WebContent/app/app.js
+++ ProjekatWebProgramiranje/WebContent/app/app.js
@@ -18,31 +18,7 @@ var app = new Vue({
     		.post("rest/users/register", user)
     		.then(response => toast('User ' + user.name + " " + user.surname + " uspešno kreiran."));
     		this.mode = 'BROWSE';
-		},
-    	selectStudent : function(student) {
-    		if (this.mode == 'BROWSE') {
-    			this.selectedStudent = student;
-    		}    
-    	},
-    	editStudent : function() {
-    		if (this.selectedStudent.jmbg == undefined)
-    			return;
-    		this.backup = [this.selectedStudent.ime, this.selectedStudent.prezime, this.selectedStudent.brojIndeksa, this.selectedStudent.datumRodjenja];
-    		this.mode = 'EDIT';
-    	},
-    	updateStudent : function(student) {
-    		axios
-    		.post("rest/studenti/updatejson", student)
-    		.then(response => toast('Student ' + student.ime + " " + student.prezime + " uspešno snimljen."));
-    		this.mode = 'BROWSE';
-    	},
-    	cancelEditing : function() {
-    		this.selectedStudent.ime = this.backup[0];
-    		this.selectedStudent.prezime = this.backup[1];
-    		this.selectedStudent.brojIndeksa = this.backup[2];
-    		this.selectedStudent.datumRodjenja = this.backup[3];
-    		this.mode = 'BROWSE';
-    	}
+		}
     },
     filters: {
     	dateFormat: function (value, format) {
--- ProjekatWebProgramiranje/src/services/UserService.java
+++ ProjekatWebProgramiranje/src/services/UserService.java
@@ -1,7 +1,10 @@
 package services;
 
-import javax.ws.rs.Consumes;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -12,10 +15,17 @@ import beans.User;
 public class UserService {
 	
 	@GET
-	@Path("/register")
+	@Path("/getalljson")
 	@Produces(MediaType.APPLICATION_JSON)
-	@Consumes(MediaType.APPLICATION_JSON)
+	public List<User> getUsers() {
+		List<User>users = new ArrayList<User>();
+		return users;
+	}
+	
+	@POST
+	@Path("/register")
+	@Produces(MediaType.TEXT_PLAIN)
 	public void RegisterUser(User user) {
-		System.out.println("saving user: " + user);
+		System.out.println("saving user: " + user.getName());
 	}
 }
