b07de01d826dc0df742243341d0df3516cd00e0b
Stefann11 <santrac.stefan@gmail.com>
2020-08-21 13:21:07 +0200

    Promena projekta

---
+++ ASProjekatWeb/ASProjekatWeb/src/services/UserService.java
@@ -0,0 +1,45 @@
+package services;
+
+import java.util.Collection;
+
+import javax.annotation.PostConstruct;
+import javax.servlet.ServletContext;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+
+import beans.User;
+import dao.UserDAO;
+
+@Path("/users")
+public class UserService {
+	
+	@Context
+	ServletContext ctx;
+	
+	public UserService() {
+	}
+	
+	@PostConstruct
+	// ctx polje je null u konstruktoru, mora se pozvati nakon konstruktora (@PostConstruct anotacija)
+	public void init() {
+		// Ovaj objekat se instancira više puta u toku rada aplikacije
+		// Inicijalizacija treba da se obavi samo jednom
+		if (ctx.getAttribute("userDAO") == null) {
+	    	String contextPath = ctx.getRealPath("");
+			ctx.setAttribute("userDAO", new UserDAO(contextPath));
+		}
+	}
+	
+	@GET
+	@Path("/")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Collection<User> getProducts(){
+		UserDAO dao = (UserDAO) ctx.getAttribute("userDAO");
+		return dao.findAll();
+	}
+	
+	
+}
