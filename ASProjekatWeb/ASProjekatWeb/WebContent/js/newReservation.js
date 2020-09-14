1ef82eeeab007c4efcc5972df24a7f0845958eb9
Stefann11 <santrac.stefan@gmail.com>
2020-09-14 23:51:03 +0200

    Uradjeno blokiranje rezervacija za zauzete termine

--- ASProjekatWeb/ASProjekatWeb/WebContent/js/newReservation.js
+++ ASProjekatWeb/ASProjekatWeb/WebContent/js/newReservation.js
@@ -1,38 +1,7 @@
 $(document).ready(function() {
 	createApartment();
 	addDatesToApartment();
-});
-
-
-/*availableDates = ['24-09-2020','25-09-2020'];
-
-function initDatePicker(){
-	$("#datePicker").datepicker({
-    beforeShowDay: function(d) {
-        var dmy = (d.getMonth()+1); 
-        if(d.getMonth()<9) 
-            dmy="0"+dmy; 
-        dmy+= "-"; 
-        
-        if(d.getDate()<10) dmy+="0"; 
-            dmy+=d.getDate() + "-" + d.getFullYear(); 
-        
-        console.log(dmy+' : '+($.inArray(dmy, availableDates)));
-        
-        if ($.inArray(dmy, availableDates) != -1) {
-            return [true, "","Available"]; 
-        } else{
-             return [false,"","unAvailable"]; 
-        }
-    }
-    });
-
-//$("#dateRetrait").datepicker({buttonImage: "../../../Images/boutons/btn_calendier.png"});
-//$("#dateRetrait").datepicker({showButtonPanel: true });
-//$("#dateRetrait").datepicker({beforeShow: function() {setTimeout(function() {$(".ui-datepicker").css("z-index", 9999999999);}, 10);}});
-
-}*/
-
+});s
 
 
 
@@ -54,31 +23,48 @@ function createApartment(){
 
 		
 		var startDate = new Date(+dateParts[2], dateParts[1] - 1, +dateParts[0]); 
+		
+		/*var flag = 0;
+		
+		for (var i=0; i<listDates.length;i++){
+			var dateParts2 = listDates[i].split("-");
+			var thisDate = new Date(+dateParts2[2], dateParts2[1] - 1, +dateParts2[0]);
+			
+			if (thisDate.getTime()===startDate.getTime()){
+				flag = 1;
+				break;
+			} 
+		}
+		
+		if (flag == 0){*/
 
 		
-			var $inputs = $('#apartmentForm input:not([type="submit"])');
-			var values = {};
-			$inputs.each(function() {
-				values[this.name] = $(this).val();
-			});
-					
-			$.ajax({
-				type: "POST",
-				url: "rest/reservations/save",
-				contentType : "application/json",
-				accept: "application/json",
-				data: JSON.stringify({id: id, apartment: apartment, startDate: startDate, numberOfOvernights: numberOfOvernights, message: message}),
-				success: function(data, textStatus, XmlHttpRequest){
-					toastr["success"]("Uspešno kreiranje!");
-					setTimeout(function() {
-						window.location.assign( XmlHttpRequest.responseText);
-						$('#apartmentForm')[0].reset();
-					}, 1000);
-				},
-				error: function(jqXHR, textStatus, errorThrown)  {
-					toastr["error"](jqXHR.responseText);
-				}
+				var $inputs = $('#apartmentForm input:not([type="submit"])');
+				var values = {};
+				$inputs.each(function() {
+					values[this.name] = $(this).val();
+				});
+						
+				$.ajax({
+					type: "POST",
+					url: "rest/reservations/save",
+					contentType : "application/json",
+					accept: "application/json",
+					data: JSON.stringify({id: id, apartment: apartment, startDate: startDate, numberOfOvernights: numberOfOvernights, message: message}),
+					success: function(data, textStatus, XmlHttpRequest){
+						toastr["success"]("Uspešno kreiranje!");
+						setTimeout(function() {
+							window.location.assign( XmlHttpRequest.responseText);
+							$('#apartmentForm')[0].reset();
+						}, 1000);
+					},
+					error: function(jqXHR, textStatus, errorThrown)  {
+						toastr["error"](jqXHR.responseText);
+					}
 		});
+		/*} else {
+			alert("Zauzeti datumi");
+		}*/
 	});
 }
 
